4ecd23b76ea1bf7c96d933f7c1d996d07150c7ff
roberte777 <robewilkes@yahoo.com>
2022-12-11 13:48:37 -0600

    feat: converted to floating window

--- lua/keep-it-secret/init.lua
+++ lua/keep-it-secret/init.lua
@@ -13,6 +13,35 @@ function M.toggle()
 	M.enabled = not M.enabled
 end
 
+Win_id = nil
+Buf_id = nil
+Filename = nil
+local function create_win()
+	local width = 60
+	local height = 10
+	local buf = vim.api.nvim_create_buf(false, true)
+	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+	--create centered floating window
+	local winid, win = require("plenary.popup").create(buf, {
+		minwidth = width,
+		minheight = height,
+		line = math.floor(((vim.o.lines - height) / 2) - 1),
+		col = math.floor((vim.o.columns - width) / 2),
+		style = "minimal",
+		borderchars = borderchars,
+	})
+	--set buffer contents
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, {
+		"WARNING!:",
+		"",
+		"This file contains sensitive information.",
+		"If you are sharing your screen, you should not enter.",
+		"Press 'y' to enter or 'n' to quit.",
+	})
+	return { popup_bufnr = buf, popup_winid = winid }
+end
+
+--function runs on the BufAdd event
 function M.show_warning_if_needed()
 	if not M.enabled then
 		return
@@ -23,16 +52,36 @@ function M.show_warning_if_needed()
 	-- Check if the filename matches any of the configured wildcards
 	for _, wildcard in ipairs(M.wildcards) do
 		if string.match(filename, wildcard) then
-			local choice = vim.fn.confirm(
-				"Warning! You are about to show a file that could contain secrets. Would you like to continue?",
-				"&Yes\n&No"
-			)
-			if choice == 2 then
-				vim.api.nvim_command("bd")
+			if Filename and string.match(filename, Filename) then
+				Filename = nil
+				return
 			end
+			Filename = filename
+			-- move user to empty buffer
+			local buf = vim.api.nvim_create_buf(false, true)
+			vim.api.nvim_set_current_buf(buf)
+
+			local win_info = create_win()
+			Win_id = win_info.popup_winid
+			Buf_id = win_info.popup_bufnr
+			-- set keymap to close window
+			vim.api.nvim_buf_set_keymap(
+				Buf_id,
+				"n",
+				"y",
+				--close window and temp buffer, move user to original buffer
+				":q<CR>:bd<CR>",
+				{ noremap = true, silent = true }
+			)
+			-- set keymap for "n" if user does not want to enter secret file
+			vim.api.nvim_buf_set_keymap(Buf_id, "n", "n", ":q<CR>:bd<CR>:bd<CR>", { noremap = true, silent = true })
 			break
 		end
 	end
+end
+
+function M.close_menu()
+	vim.api.nvim_win_close(Win_id, true)
 end
 
 return M
--- plugin/init.lua
+++ plugin/init.lua
@@ -9,6 +9,6 @@
 -- vim.api.nvim_create_autocmd
 vim.api.nvim_create_autocmd("BufEnter", {
 	callback = function()
-		require("keep-it-secret").show_warning_if_needed(".env")
+		require("keep-it-secret").show_warning_if_needed()
 	end,
 })
